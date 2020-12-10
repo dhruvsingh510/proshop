2ab558bc72fd105f4ecfcd452985e970d7c93ba5
Dhruv Singh <dhruvsingh510@gmail.com>
2020-12-10 23:19:09 +0530

    adding static products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
- import { Container } from 'react-bootstrap' 
- import Header from './components/Header'
- import Footer  from './components/Footer'
- 
- const App = () => {
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
+const App = () => {
   return (
     <>
-    <Header />
-      <main className='py-3'>
+      <Header />
+      <main className="py-3">
         <Container>
-        <h1>Welcome to ProShop</h1> 
+          <HomeScreen />
         </Container>
-      
       </main>
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
