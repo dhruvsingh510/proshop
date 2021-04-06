7b22933c043151a09bb5665e0894461a543e6d09
Dhruv Singh <dhruvsingh510@gmail.com>
2021-04-06 22:30:29 +0530

    Admin product edit screen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,16 +2,19 @@ import express from 'express'
 import {
   getProducts,
   getProductById,
-  deleteProduct
+  deleteProduct,
+  createProduct,
+  updateProduct
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-// @desc    Fetch all products
-router.route('/').get(getProducts)
-
-// @desc    Fetch single product
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,6 +9,9 @@ import {
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -56,23 +59,23 @@ export const listProductDetails = (id) => async (dispatch) => {
 export const deleteProduct = (id) => async (dispatch, getState) => {
   try {
     dispatch({
-      type: PRODUCT_DELETE_REQUEST,
+      type: PRODUCT_DELETE_REQUEST
     })
 
     const {
-      userLogin: { userInfo },
+      userLogin: { userInfo }
     } = getState()
 
     const config = {
       headers: {
-        Authorization: `Bearer ${userInfo.token}`,
-      },
+        Authorization: `Bearer ${userInfo.token}`
+      }
     }
 
     await axios.delete(`/api/products/${id}`, config)
 
     dispatch({
-      type: PRODUCT_DELETE_SUCCESS,
+      type: PRODUCT_DELETE_SUCCESS
     })
   } catch (error) {
     dispatch({
@@ -80,7 +83,40 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
+          : error.message
+    })
+  }
+}
+
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REQUEST
+    })
+
+    const {
+      userLogin: { userInfo }
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.post(`/api/products`, {}, config)
+
+    dispatch({
+      type: PRODUCT_CREATE_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
     })
   }
 }
